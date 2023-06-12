d7b2e281e54022f0d3a93495c4d93ccbde169995
Hernande <80050052+Nande14@users.noreply.github.com>
2023-06-12 17:01:59 -0300

    Update Runas.js

--- src/components/pages/Runas.js
+++ src/components/pages/Runas.js
@@ -1,13 +1,1 @@
-import styles from './Runas.module.css'
-import tela from '../../img/TelaRunas.png'
-function Runas(){
-return(
-    
-    <section className={styles.runas_container}>
-        <img src={tela} alt="Tela runas"></img>
-    </section>
-)
 
-}
-
-export default Runas
