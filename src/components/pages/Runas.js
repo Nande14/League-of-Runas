a9b88bf5f1813053caafc5ccb341500a2de2add5
Hernande <80050052+Nande14@users.noreply.github.com>
2023-06-12 14:46:02 -0300

    Create Runas.js

---
+++ src/components/pages/Runas.js
@@ -0,0 +1,13 @@
+import styles from './Runas.module.css'
+import tela from '../../img/TelaRunas.png'
+function Runas(){
+return(
+    
+    <section className={styles.runas_container}>
+        <img src={tela} alt="Tela runas"></img>
+    </section>
+)
+
+}
+
+export default Runas
