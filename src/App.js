a7f26a93819efd6ea73ff7e5afe405346d257bd0
Hernande <80050052+Nande14@users.noreply.github.com>
2023-06-12 14:26:36 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,36 +1,58 @@
 import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom"
 import Home from './components/pages/Home'
-import Sobre from './components/pages/Runas'
-import Contatos from './components/pages/GitHub'
+import Runas from './components/pages/Runas'
+import Feiticos from "./components/pages/Feiticos"
+import Partida from "./components/pages/Partida"
+import Login from "./components/pages/Login"
 import Adicionar from "./components/pages/Adicionar"
+
 
 import Container from './components/layouts/Container'
 import Navbar from './components/layouts/Navbar'
 import Footer from './components/layouts/Footer'
 
+
 function App() {
   return (
-    <Router>
-      <Navbar/>
-      <Switch>
-      <Container customClass="minHeight">
+      <Router>
+        <Navbar />
+        <Switch>
+          <Container customClass="minHeight">
             <Route exact path="/">
-              <Home/>
+              <Home />
             </Route>
+
+
             <Route exact path="/runas">
-              <Sobre/>
+              <Runas />
             </Route>
-            <Route exact path="/gitHub">
-              <Contatos/>
+
+
+            <Route exact path="/feiticos">
+              <Feiticos />
+              </Route>
+
+              <Route exact path="/partida">
+              <Partida />
+              </Route>
+
+
+            <Route exact path="/login">
+              <Login />
             </Route>
+
             <Route exact path="/adicionar">
-              <Adicionar/>
-            </Route> 
-      </Container> 
-      </Switch>
-      <Footer/>
-    </Router>
+              <Adicionar />
+            </Route>
+            
+          </Container>
+        </Switch>
+        <Footer />
+      </Router>
   );
 }
+  
+
+
 
 export default App;
