d5a01dea32af290d36f289044eee8cac6483fe7e
Hernande <80050052+Nande14@users.noreply.github.com>
2023-06-12 17:03:15 -0300

    Update Home.js

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,13 +1,1 @@
-import styles from './Home.modules.css'
-import tela from '../../img/TelaHome.png'
-function Home(){
-return(
-    
-    <section className={styles.home_container}>
-        <img src={tela} alt="Tela home"></img>
-    </section>
-)
 
-}
-
-export default Home
